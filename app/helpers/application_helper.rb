db1ffb7b56d6b62bdd5745821b0a42dfa2d8ddf9
Ahsan Sharafuddin <ahsan.sharafuddin@hotmail.com>
2011-05-15 02:14:50 -0400

    End of Chapter 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 
-  # Return a title on a per-page basis.
+  def logo
+    @logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
+# Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
@@ -9,4 +13,5 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
 end
