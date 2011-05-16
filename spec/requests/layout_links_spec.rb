4111084fcf09053e91a5f73c7cb1029521936a98
Ahsan Sharafuddin <ahsan.sharafuddin@hotmail.com>
2011-05-15 21:54:03 -0400

    Finished first cut of the User model

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -24,7 +24,7 @@ describe "LayoutLinks" do
   
   it "should have a signup page at '/signup'" do
     get '/signup'
-    response.should have_selector('title', :content => "Sign up")
+    response.should have_selector('title', :content => "Sign up now!")
   end
   
   it "should have the right links on the layout" do
